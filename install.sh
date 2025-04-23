11ae391f47452a1110ac07eb724271daf337143d
Valentin-rayt <valentin.rayet.online@gmail.com>
2025-04-23 17:07:28 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-apt-get update
-apt-get install -y gcc g++ libc6-dev
-pip install -r requirements.txt
+apt-get update && \
+apt-get install -y wget gnupg && \
+wget -qO- https://deb.nodesource.com/setup_18.x | bash - && \
+apt-get install -y nodejs && \
+npm i -g playwright && \
+npx playwright install
